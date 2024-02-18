fe502803951a06b1999f96c41757cca0fd6e85d3
isabellaqian <isabellajqian@gmail.com>
2024-02-17 22:39:25 -0800

    basic skeleton

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,46 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { roboto } from "./ui/fonts";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+    <div className={styles.main}>
+      <div
+        className={styles.areYouReady}
+      >{`Are you ready to ace your next job interview with confidence? Say hello to Ai&U – your personalized interview preparation assistant powered by cutting-edge artificial intelligence technology.`}</div>
+      <div className={styles.frame}>
+        <div className={styles.boostYourConfidenceContainer}>
+          <p className={styles.boostYour}>Boost your</p>
+          <p className={styles.boostYour}>{`confidence `}</p>
+          <p className={styles.withAiu}>
+            <span className={styles.with}>{`with `}</span>
+            <span className={styles.aiu}>{`Ai&U`}</span>
           </p>
-        </a>
+        </div>
+        <div className={styles.experienceNowWrapper}>
+          <Link href={"/interview"}>
+            <div className={styles.experienceNow}>Experience Now</div>
+          </Link>
+        </div>
+        <div className={styles.anAiPowered}>
+          An AI powered platform to help you ace your interviews
+        </div>
+        <Image
+          className={styles.image9Icon}
+          src={"/landing2.png"}
+          width="440"
+          height="280"
+        />
+        <Image
+          className={styles.image8Icon}
+          src={"/landing1.png"}
+          width="490"
+          height="320"
+        />
+        {/* <img className={styles.image9Icon} alt="" src="/image-9@2x.png" />
+        <img className={styles.image8Icon} alt="" src="/image-8@2x.png" /> */}
       </div>
-    </main>
+    </div>
   );
 }
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Roboto, Quicksand } from 'next/font/google';
+ 
+export const roboto = Roboto({ subsets: ['latin'], weight: '400' });
+export const quicksand = Quicksand({ subsets: ['latin'], weight: '400' });
